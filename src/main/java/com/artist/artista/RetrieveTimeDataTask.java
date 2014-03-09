5b51296a0153e98380c7dd2cb3eb0e5ac17097f5
ryoppei <felipe.crespo@ventura24.es>
2014-03-09 18:58:55 +0100

    ARTiSTA-android-1, Adapting to new ARTiST-core API

--- src/main/java/com/artist/artista/RetrieveTimeDataTask.java
+++ src/main/java/com/artist/artista/RetrieveTimeDataTask.java
@@ -1,19 +1,29 @@
 package com.artist.artista;
 
 import android.os.AsyncTask;
+import com.artist.Country;
 import com.artist.TransportScheduleDAO;
+import com.artist.vo.ArrivalData;
 
+import java.util.ArrayList;
 import java.util.List;
 
-class RetrieveTimeDataTask extends AsyncTask<String, Void, List<Long>>
+class RetrieveTimeDataTask extends AsyncTask<String, Void, List<ArrivalData>>
 {
 
     private Exception exception;
 
     @Override
-    protected List<Long> doInBackground(String... params)
+    protected List<ArrivalData> doInBackground(String... params)
     {
-        return TransportScheduleDAO.INSTANCE.findNext();
+        final List<String> lines = new ArrayList<String>();
+        lines.add("10");
+
+        return TransportScheduleDAO.INSTANCE.findNext(Country.SPAIN,
+                                                      "EMT_MADRID",
+                                                      "5442",
+                                                      null,
+                                                      lines);
     }
 
 }
